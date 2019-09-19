8eb1c89474d25c469f41995f10833c897d60232f
icemiliang <icemiliang@gmail.com>
2019-09-19 17:32:38 -0400

    update

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ This package includes the prototype code for implementing spherical harmonic map
 
 [![Build status](https://ci.appveyor.com/api/projects/status/6nyv0sobm4k0ey2j?svg=true)](https://ci.appveyor.com/project/icemiliang/spherical-harmonic)
 
-
 In the root directory, run:
 ```
 $ rm -r build
@@ -24,6 +23,8 @@ The program has been tested on Ubuntu 16.04 with g++ 5.4.0.
 ```
 ./main ../data/brain.obj output
 ```
+
+The output is three files: star.obj tuette.obj and harmonic.obj. star is the initial map that projects all vertices onto the unit sphere. Tuette map is an intermidiate step. Harmonic map is the final result.
 
 ## References
 Gu, Xianfeng David. Computational conformal geometry. Edited by Shing-Tung Yau. Somerville, Mass, USA: International Press, 2008.
--- main.cpp
+++ main.cpp
@@ -11,27 +11,31 @@ int main(int argc, char *argv[]) {
 	OBJFileReader of;
 	ifstream in(argv[1]);
 	of.readToSolid(&nmesh, in);
-	Solid * pnmesh = &nmesh;
 
-	Harmonic shm;
+	Harmonic shm(&nmesh);
 	cout << "--> Setting up kuv" << endl;
-	shm.set_up(pnmesh);
+	shm.set_up();
 
-	cout << "--> Gauss map" << endl;
-	shm.star_map(&nmesh);
+	cout << "--> Star map" << endl;
+	shm.star_map();
 	string out = string(argv[2]) + "_star.obj";
 	of.writeToObj(&nmesh, out);
 
 	cout << "--> Computing Tuette map" << endl;
-	shm.tuette_map(&nmesh);
+	shm.tuette_map();
 	out = string(argv[2]) + "_tuette.obj";
 	of.writeToObj(&nmesh, out);
 
 	cout << "--> Computing harmonic map" << endl;
-	// shm.harmonic_map_gd(&nmesh);
-	shm.harmonic_map_conjugate_gd(&nmesh);
+	// shm.harmonic_map_gd();
+	shm.harmonic_map_conjugate_gd();
 	out = string(argv[2]) + "_harmonic.obj";
 	of.writeToObj(&nmesh, out);
+
+	cout << "--> Checking angle distorsions" << endl;
+	// vector<double> errors = shm.check_conformality();
+
+	
 
 	return 0;
 }
